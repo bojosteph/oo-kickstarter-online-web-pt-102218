96e00b5eb437bd164eef9a94e45b75610fbc4329
Roberto Samia <bojosteph@gmail.com>
2018-11-21 01:16:17 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,14 @@
 class Backer
+
   attr_accessor :name, :backed_projects
-   
-   
+
   def initialize(name)
     @name = name
     @backed_projects = []
-    
   end
-  
-  def backed_project(project)
-    
+
+  def back_project(project)
     @backed_projects << project
-   project.add_backer(name) << self
+    project.backers << self
   end
 end
